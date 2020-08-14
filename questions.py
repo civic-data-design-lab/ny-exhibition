954004ff3d387ea13f00e37f22b55a0b15919881
Adam Janicki <adamjanicki2@gmail.com>
2020-08-14 13:12:05 -0400

    Update questions.py

--- questions.py
+++ questions.py
@@ -1,6 +1,6 @@
-questions = [
+questions = {'temporary_theme': [
   'Parking spaces in NYC should be used for',
   'Design can have an impact on climate change if we',
   'The right to housing means',
   'A healthy city is'
-]
+]}
