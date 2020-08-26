a3adea0d939e48f228a5e2e1d29948c77a048765
Adam Janicki <adamjanicki2@gmail.com>
2020-08-26 15:48:52 -0400

    new themes/questions

--- questions.py
+++ questions.py
@@ -1,8 +1,25 @@
 questions = {
-  'temporary_theme': [
-    'Parking spaces in NYC should be used for',
-    'Design can have an impact on climate change if we',
-    'The right to housing means',
-    'A healthy city is'
+  'Evolving Public Realm': [
+    'One policy/project to make "waste not-want not" a reality for NYC buildings and public spaces would be',
+    'By embracing my neighborhood, I’m motivated to',
+    'Care includes all that we do to maintain, repair, heal, and sustain our communities and environment. We can best care for our city by'
+  ],
+  'Climate Change and Resilience': [
+    'Design for inclusive urban landscapes is',
+    "New York City's 520 miles of coastline can become a continuous public realm and adapt nimbly to rising sea levels by",
+    'The design and operation of buildings contributes a majority of NYC’s greenhouse gas emissions.  We can make buildings part of our climate solution by'
+  ],
+  'Right to Housing': [
+    'To combat structural and systemic racism and undo the legacy of redlining, NYC must',
+    'Housing can play a role in helping NYC recover from the pandemic and other disasters in the future by',
+    'When I think about the neighborhoods I have lived in and how they have influenced my life, it makes me realize that the right to housing is',
+    'Home is where I'
+
+  ],
+  'Public Health': [
+    'After the pandemic New York will be',
+    'We can cultivate, catalyze, and amplify Black joy in New York City by',
+    'Neighborhood inequality undermines the health of all New Yorkers. We can begin to reconnect and share the wealth by',
+    'The pandemic and social and racial unrest amplified the importance of the public realm in New York, we can apply these lesson to our future city by'
   ]
 }
